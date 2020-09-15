1f40e0e6f4c57b6507e1b5ec5a6f1daaf43abc1f
clumsy-k <1960.do@gmail.com>
2020-09-16 06:52:08 +0900

    edit

---
+++ scatter_diagram.R
@@ -0,0 +1,42 @@
+# A basic scatter diagram displays the relationship between two continuous value variables.
+library(ggplot2)
+library(gcookbook) # heightweight
+library(dplyr)
+
+# I want to create a scatterplot using two continuous value variables.
+# defalt size : 2
+# change size and shape
+# shape : 0 ~ 25, "*", ".", "o", "O", "0", "+", "-", "|", "%", "#"
+g <- ggplot(heightweight, aes(x = ageYear, y = heightIn)) + geom_point(shape = 21, size = 1.5)
+plot(g)
+
+# I want to use a variable (for grouping) to visibly group points by color or shape.
+# change colour and shape
+g <- ggplot(heightweight, aes(x = ageYear, y = heightIn)) + geom_point() + 
+  scale_shape_manual(values = c(1,2)) + scale_colour_brewer(palette = "Set1")
+plot(g)
+
+# I want to set the shape of a point to be different from the default.
+# make data.frame : weight < 100, weight >= 100
+hw <- heightweight %>% 
+  mutate(weightgroup = ifelse(weightLb < 100, "< 100", ">= 100"))
+# shape = sex, fill = weightgroup, colour = (NA, "black")
+g <- ggplot(hw, aes(x = ageYear, y = heightIn, shape = sex, fill = weightgroup)) + 
+  geom_point(size = 2.5) + 
+  scale_shape_manual(values = c(21, 24)) + 
+  scale_fill_manual(
+    values = c(NA, "black"), 
+    guide = guide_legend(override.aes = list(shape = 21))
+  )
+plot(g)
+
+# I want to represent the color and size of a point using a third continuous value variable.
+# I can use guide_legend() to use a discrete legend instead of a color bar.
+g <- ggplot(heightweight, aes(x = ageYear, y = heightIn, fill = weightLb)) + 
+  geom_point(shape = 21, size = 2.5) + 
+  scale_fill_gradient(
+    low = "black", high = "white", 
+    breaks = seq(70, 170, by = 20), 
+    guide = guide_legend()
+  )
+plot(g)
